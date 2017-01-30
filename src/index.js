e65a6d493f0a9f0931a01e76db2d326d5d3cf6e5
TomFalkner <tafalkner@gmail.com>
2017-01-30 15:01:31 -0800

    Update to lodash v4.x

--- src/index.js
+++ src/index.js
@@ -125,13 +125,20 @@ const plugin = postal.fedx.transports.xframe = {
 	},
 	disconnect: function( options ) {
 		options = options || {};
+		const that = this;
 		const clients = options.instanceId ?
 			// an instanceId value or array was provided, let's get the client proxy instances for the id(s)
-			_.reduce( _.isArray( options.instanceId ) ? options.instanceId : [ options.instanceId ], _memoRemoteByInstanceId, [], this ) :
+			_.reduce( _.isArray( options.instanceId ) ? options.instanceId : [ options.instanceId ], function( memo, target ) {
+				_memoRemoteByInstanceId.call( that, memo, target );
+				return memo;
+			}, [] ) :
 			// Ok so we don't have instanceId(s), let's try target(s)
 			options.target ?
 				// Ok, so we have a targets array, we need to iterate over it and get a list of the proxy/client instances
-				_.reduce( _.isArray( options.target ) ? options.target : [ options.target ], _memoRemoteByTarget, [], this ) :
+				_.reduce( _.isArray( options.target ) ? options.target : [ options.target ], function( memo, target ) {
+					_memoRemoteByTarget.call( that, memo, target );
+					return memo;
+				}, [] ) :
 				// aww, heck - we don't have instanceId(s) or target(s), so it's ALL THE REMOTES
 				this.remotes;
 		if ( !options.doNotNotify ) {
@@ -143,6 +150,7 @@ const plugin = postal.fedx.transports.xframe = {
 		targets = _.isArray( targets ) ? targets : [ targets ];
 		targets = targets.length ? targets : this.getTargets();
 		callback = callback || NO_OP;
+		var remotes = this.remotes;
 		_.each( targets, function( def ) {
 			if ( def.target ) {
 				def.origin = def.origin || state.config.defaultOriginUrl;
@@ -151,11 +159,11 @@ const plugin = postal.fedx.transports.xframe = {
 				} );
 				if ( !remote ) {
 					remote = XFrameClient.getInstance( def.target, def.origin );
-					this.remotes.push( remote );
+					remotes.push( remote );
 				}
 				remote.sendPing( callback );
 			}
-		}, this );
+		} );
 	},
 	addEventListener: env.isWorker ? function() {
 		addEventListener( "message", listener );
